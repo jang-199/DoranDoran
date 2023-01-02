6b8a2e9039041db894fe3de60d1068cf90db7981
jang-199 <jw1010110@gmail.com>
2023-01-02 22:36:25 +0900

    Initial commit

---
+++ doranserver/src/main/java/com/dorandoran/doranserver/DoranserverApplication.java
@@ -0,0 +1,13 @@
+package com.dorandoran.doranserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoranserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoranserverApplication.class, args);
+	}
+
+}
---
+++ doranserver/src/test/java/com/dorandoran/doranserver/DoranserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dorandoran.doranserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoranserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

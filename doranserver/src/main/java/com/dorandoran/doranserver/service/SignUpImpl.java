38cf0e19927db502b3d259960ba06ff0808286ac
LeeKing2017 <thrusum123@gmail.com>
2023-01-11 00:08:43 +0900

    feat:add Service
    Signed-off-by: LeeKing2017 <thrusum123@gmail.com>

---
+++ doranserver/src/main/java/com/dorandoran/doranserver/service/SignUpImpl.java
@@ -0,0 +1,21 @@
+package com.dorandoran.doranserver.service;
+
+import com.dorandoran.doranserver.entity.Member;
+import com.dorandoran.doranserver.entity.PolicyTerms;
+
+public class SignUpImpl implements SignUp{
+    @Override
+    public void saveMember(Member member) {
+
+    }
+
+    @Override
+    public Boolean checkNickname(Member member) {
+        return null;
+    }
+
+    @Override
+    public Boolean checkPolicyTerms(PolicyTerms policyTerms) {
+        return null;
+    }
+}

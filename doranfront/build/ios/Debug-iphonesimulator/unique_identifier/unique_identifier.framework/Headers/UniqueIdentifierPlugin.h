75c1e15764fc858a52d935cba48652063bd42981
lsuinl <su96in43@naver.com>
2023-01-17 23:08:42 +0900

    ignore

--- doranfront/build/ios/Debug-iphonesimulator/unique_identifier/unique_identifier.framework/Headers/UniqueIdentifierPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface UniqueIdentifierPlugin : NSObject<FlutterPlugin>
-@end
--- doranfront/lib/const/storage.dart
+++
@@ -1,7 +0,0 @@
-String? firebasetoken;
-String? kakaotoken;
-
-//서버 url
-final url = Uri.parse('http://116.44.231.162:8080/api/signup');
-
-String kakaonativekey = 'd54cd76337470f87b093bfdadfa53292';

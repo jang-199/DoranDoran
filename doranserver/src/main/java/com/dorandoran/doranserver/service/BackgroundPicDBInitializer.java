74b7ec473d2359a64deb8cd09896c219b35542c1
jang-199 <jw1010110@gmail.com>
2023-02-17 23:42:07 +0900

    TEST: TEST DATA 생성

--- doranserver/src/main/java/com/dorandoran/doranserver/service/BackgroundPicDBInitializer.java
+++ doranserver/src/main/java/com/dorandoran/doranserver/service/BackgroundPicDBInitializer.java
@@ -85,26 +85,154 @@ public class BackgroundPicDBInitializer {
 //    }
 
     @PostConstruct
-    public void datainit() {
-        for (int i = 0; i < 500; i++) {
-            PolicyTerms build = PolicyTerms.builder().policy1(true).policy2(true).policy3(true).build();
-            policyTermsCheck.policyTermsSave(build);
-            Member buildMember = Member.builder().policyTermsId(build)
-                    .email(i + "@gmail.com")
-                    .dateOfBirth(LocalDate.now())
-                    .firebaseToken("firebasetoken")
-                    .nickname("nickname" + i)
-                    .signUpDate(LocalDateTime.now()).build();
-            memberService.saveMember(buildMember);//회원 500명 생성
+    public void testData(){
 
-            Post.builder().content("회원" + i + "의 글입니다.")
-                    .forMe(false)
-                    .location("37.2326962,127.1877412")
-                    .postTime(LocalDateTime.now())
-                    .memberId(buildMember)
-                    .switchPic(ImgType.DefaultBackground)
-                    .ImgName(i/5+".png")
+        UserUploadPic build8 = UserUploadPic.builder().imgName("6.jpg").serverPath(serverPath + "6.jpg").build();
+        userUploadPicService.saveUserUploadPic(build8);
+
+        PolicyTerms build = PolicyTerms.builder().policy1(Boolean.TRUE).policy2(Boolean.TRUE).policy3(Boolean.TRUE).build();
+        policyTermsCheck.policyTermsSave(build);
+
+        Member member1 = Member.builder()
+                .email("1")
+                .dateOfBirth(LocalDate.now())
+                .nickname("1")
+                .firebaseToken("1")
+                .signUpDate(LocalDateTime.now())
+                .policyTermsId(build)
+                .build();
+
+        Member member2 = Member.builder()
+                .email("2")
+                .dateOfBirth(LocalDate.now())
+                .nickname("2")
+                .firebaseToken("1")
+                .signUpDate(LocalDateTime.now())
+                .policyTermsId(build)
+                .build();
+
+        Member member3 = Member.builder()
+                .email("3")
+                .dateOfBirth(LocalDate.now())
+                .nickname("3")
+                .firebaseToken("1")
+                .signUpDate(LocalDateTime.now())
+                .policyTermsId(build)
+                .build();
+
+        memberService.saveMember(member1);
+        memberService.saveMember(member2);
+        memberService.saveMember(member3);
+
+        Post post1 = Post.builder().
+                postTime(LocalDateTime.now())
+                .postId(1L)
+                .forMe(Boolean.FALSE)
+                .content("1")
+                .location("1")
+                .switchPic(ImgType.DefaultBackground)
+                .ImgName("6.jpg")
+                .memberId(member1)
+                .build();
+
+        Post post2 = Post.builder().
+                postTime(LocalDateTime.now())
+                .postId(2L)
+                .forMe(Boolean.FALSE)
+                .content("2")
+                .location("1")
+                .switchPic(ImgType.DefaultBackground)
+                .ImgName("6.jpg")
+                .memberId(member2)
+                .build();
+
+        Post post3 = Post.builder().
+                postTime(LocalDateTime.now())
+                .postId(3L)
+                .forMe(Boolean.FALSE)
+                .content("3")
+                .location("1")
+                .switchPic(ImgType.UserUpload)
+                .ImgName("6.jpg")
+                .memberId(member3)
+                .build();
+        postService.savePost(post1);
+        postService.savePost(post2);
+        postService.savePost(post3);
+
+        //멤버1
+        PostLike build1 = PostLike.builder().postId(post1).memberId(member1).build();
+
+        //멤버2
+        PostLike build2 = PostLike.builder().postId(post1).memberId(member2).build();
+        PostLike build3 = PostLike.builder().postId(post2).memberId(member2).build();
+
+        //멤버3
+        PostLike build4 = PostLike.builder().postId(post1).memberId(member3).build();
+        PostLike build5 = PostLike.builder().postId(post2).memberId(member3).build();
+        PostLike build6 = PostLike.builder().postId(post3).memberId(member3).build();
+
+        postLikeService.savePostLike(build1);
+        postLikeService.savePostLike(build2);
+        postLikeService.savePostLike(build3);
+        postLikeService.savePostLike(build4);
+        postLikeService.savePostLike(build5);
+        postLikeService.savePostLike(build6);
+
+
+    }
+
+
+    @PostConstruct
+    public void init() {
+
+        for (int i = 0; i < max; i++) {
+            BackgroundPic build = BackgroundPic.builder().imgName(i+1 + ".jpg")
+                    .serverPath(serverPath + (i+1) + ".jpg")
                     .build();
+            backGroundPicService.saveBackgroundPic(build);
+        }
+
+
+
+        for (int i = 4; i <= 400+4; i++) {
+
+            PolicyTerms build1 = PolicyTerms.builder().policy1(true)
+                    .policy2(true)
+                    .policy3(true)
+                    .build();
+            policyTermsCheck.policyTermsSave(build1);
+
+            Member build2 = Member.builder().email(i + "@gmail.com")
+                    .dateOfBirth(LocalDate.now())
+                    .nickname("Im" + i)
+                    .signUpDate(LocalDateTime.now())
+                    .firebaseToken("token")
+                    .policyTermsId(build1)
+                    .build();
+            memberService.saveMember(build2);
+
+            if ((i % 2) != 0) {
+                Post build = Post.builder().postTime(LocalDateTime.now()).content("hi im" + i)
+                        .forMe(true)
+                        .location("37.2847022,127.2370257")
+                        .memberId(build2)
+                        .switchPic(ImgType.DefaultBackground)
+                        .ImgName("6.jpg")
+                        .build();
+                postService.savePost(build);
+            } else {
+                Post build = Post.builder().postTime(LocalDateTime.now()).content("hi im" + i)
+                        .forMe(false)
+                        .location("37.2847022,127.2370257")
+                        .memberId(build2)
+                        .switchPic(ImgType.UserUpload)
+                        .ImgName("6.jpg")
+                        .build();
+                postService.savePost(build);
+            }
+
         }
     }
+
 }

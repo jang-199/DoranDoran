689f7377a771b592f06cc873848d211296224e2e
jang-199 <jw1010110@gmail.com>
2023-02-17 23:31:01 +0900

    TEST: test jar 수정

--- doranserver/src/main/java/com/dorandoran/doranserver/service/BackgroundPicDBInitializer.java
+++ doranserver/src/main/java/com/dorandoran/doranserver/service/BackgroundPicDBInitializer.java
@@ -29,13 +29,63 @@ public class BackgroundPicDBInitializer {
     @Autowired
     UserUploadPicServiceImpl userUploadPicService;
 
+    @Autowired
+    CommentServiceImpl commentService;
+
     @Value("${background.cnt}")
     Integer max;
     @Value("${background.Store.path}")
     String serverPath;
 
+//    @PostConstruct
+//    public void init() {
+//
+//        for (int i = 0; i < 100; i++) {
+//            BackgroundPic build = BackgroundPic.builder().serverPath(serverPath + (i + 1) + ".png").imgName((i + 1) + ".png").build();
+//            backGroundPicService.saveBackgroundPic(build);
+//        }
+//
+//        for (Long i = 1L; i <= 500L; i++) {
+//            PolicyTerms build = PolicyTerms.builder().policy1(true).policy2(true).policy3(true).build();
+//            policyTermsCheck.policyTermsSave(build);
+//            Member buildMember = Member.builder().policyTermsId(build)
+//                    .email(i + "@gmail.com")
+//                    .dateOfBirth(LocalDate.now())
+//                    .firebaseToken("firebasetoken")
+//                    .nickname("nickname" + i)
+//                    .signUpDate(LocalDateTime.now()).build();
+//            memberService.saveMember(buildMember);//회원 500명 생성
+//
+//            Post post = Post.builder().content("회원" + i + "의 글입니다.")
+//                    .forMe(false)
+//                    .location("37.2326962,127.1877412")
+//                    .postTime(LocalDateTime.now())
+//                    .memberId(buildMember)
+//                    .switchPic(ImgType.DefaultBackground)
+//                    .ImgName((i % 100 + 1) + ".png")
+//                    .build();
+//            postService.savePost(post); //글 500개 생성
+//
+//            for (Long j = i; j >0 ; j--) {
+//                PostLike buildPostLike = PostLike.builder().memberId(buildMember).postId(postService.findSinglePost(i).get()).build();
+//                postLikeService.savePostLike(buildPostLike);
+//
+//                Comment comment = Comment.builder()
+//                        .comment("나는" + i + "야 반가워")
+//                        .commentTime(LocalDateTime.now())
+//                        .postId(postService.findSinglePost(j).get())
+//                        .memberId(buildMember).build();
+//                commentService.saveComment(comment);
+//            }
+//            //popular에 데이터 추가하고 테스트 진행
+//
+//        }
+//
+//
+//    }
+
     @PostConstruct
-    public void init() {
+    public void datainit() {
         for (int i = 0; i < 500; i++) {
             PolicyTerms build = PolicyTerms.builder().policy1(true).policy2(true).policy3(true).build();
             policyTermsCheck.policyTermsSave(build);
@@ -56,7 +106,5 @@ public class BackgroundPicDBInitializer {
                     .ImgName(i/5+".png")
                     .build();
         }
-
-
     }
 }

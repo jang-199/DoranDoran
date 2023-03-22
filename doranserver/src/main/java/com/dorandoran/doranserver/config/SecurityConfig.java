1afbe8892111f08b87dda2458c00b2577e1d7a47
LeeKing2017 <thrusum123@gmail.com>
2023-03-22 23:33:32 +0900

    REFACTOR:post-detail 익명성과 닉네임 같이 리턴하는 코드로 변경

--- doranserver/src/main/java/com/dorandoran/doranserver/config/SecurityConfig.java
+++ doranserver/src/main/java/com/dorandoran/doranserver/config/SecurityConfig.java
@@ -12,10 +12,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-/*    @Bean
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
                 .authorizeRequests()
@@ -26,33 +25,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         return httpSecurity.build();
 
-    }*/
-
-    private static final String[] PERMIT_URL_ARRAY = {
-            /* swagger v2 */
-            "/v2/api-docs",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui.html",
-            "/webjars/**",
-            /* swagger v3 */
-            "/v3/api-docs/**",
-            "/swagger-ui/**"
-    };
-
-    /**
-     * configure
-     * @param http
-     * @throws Exception
-     */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable() // swagger API 호출시 403 에러 발생 방지
-                .authorizeRequests()
-                .antMatchers(PERMIT_URL_ARRAY).permitAll()
-                .anyRequest().authenticated();
     }
 }

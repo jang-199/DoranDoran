678d13039b8503f6b633d2463fc5d8b9162b904a
LeeKing2017 <thrusum123@gmail.com>
2023-06-06 23:58:21 +0900

    FEAT:태그 검색, 사용자 즐겨찾기 태그 추가/삭제 로직 완성

--- doranserver/src/main/java/com/dorandoran/doranserver/controller/HashTagController.java
+++ doranserver/src/main/java/com/dorandoran/doranserver/controller/HashTagController.java
@@ -20,9 +20,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -40,44 +43,66 @@ public class HashTagController {
 
     @Tag(name = "해시태그 관련 API")
     @Operation(summary = "해시태그 최대 5개 조회", description = "요청한 해시태그로 시작하는 해시태그들 중 많이 사용된 5개를 반환하는 API입니다." +
-            "\n\n" + "Ex. 홍으로 해당 api을 요청하면 홍길동, 홍당무, 홍시, 홍단, 홍기처럼 홍으로 시작하는 해시태그들을 반홥합니다.")
+            "\n\n" + "Ex. 홍으로 해당 api을 요청하면 홍길동, 홍당무, 홍시, 홍단, 홍기처럼 홍으로 시작하는 해시태그들을 반환합니다.")
     @ApiResponse(responseCode = "200", description = "최대 5개의 해시태그를 리턴합니다.",
     content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = HashTagResponseDto.class)))
     @GetMapping("/hashTag")
-    public ResponseEntity<?> searchHashTag(@Parameter(description = "검색 요청하는 해시태그", required = true) @RequestParam("hashTag") String reqHashTag){
+    public ResponseEntity<?> searchHashTag(@Parameter(description = "검색 요청하는 해시태그", required = true) @RequestParam("hashTag") String reqHashTag,
+                                           @AuthenticationPrincipal UserDetails userDetails){
+        String userEmail = userDetails.getUsername();
+        List<String> memberHashes = memberHashService.findHashByEmail(userEmail);
         List<HashTag> findHashTag = hashTagService.findTop5BySearchHashTag(reqHashTag);
-        List<HashTagResponseDto> responseDto = findHashTag.stream()
-                .map(ht -> new HashTagResponseDto(ht))
-                .collect(Collectors.toList());
+        ArrayList<HashTagResponseDto> hashTagResponseDtos = new ArrayList<>();
+        for (HashTag hashTag : findHashTag) {
+            Boolean hashTagCheck = (memberHashes.contains(hashTag.getHashTagName())) ? Boolean.TRUE : Boolean.FALSE;
+            HashTagResponseDto hashTagBuild = HashTagResponseDto.builder()
+                    .hashTagName(hashTag.getHashTagName())
+                    .hashTagCount(hashTag.getHashTagCount())
+                    .hashTagCheck(hashTagCheck)
+                    .build();
+            hashTagResponseDtos.add(hashTagBuild);
+        }
 
-        return ResponseEntity.ok().body(responseDto);
+        return ResponseEntity.ok().body(hashTagResponseDtos);
     }
 
-    //토큰 받아서 사용자 email 가져오는 부분 아직 구현 못함
+    @Tag(name = "해시태그 관련 API")
+    @Operation(summary = "사용자 즐겨찾기 해시태그 추가", description = "요청한 해시태그들을 사용자 즐겨찾기 해시태그 목록에 추가하는 API입니다.")
+    @ApiResponse(responseCode = "200", description = "사용자 즐겨찾기 해시태그가 추가됩니다.")
     @PostMapping("/hashTag")
-    public ResponseEntity<?> saveHashTag(@RequestBody HashTagRequestDto hashTagRequestDto){
-        String userEmail = tokenProvider.getUserEmail(hashTagRequestDto.getToken());
+    public ResponseEntity<?> saveHashTag(@Parameter(description = "추가하려는 해시태그들") @RequestBody HashTagRequestDto hashTagRequestDto,
+                                         @AuthenticationPrincipal UserDetails userDetails){
+        String userEmail = userDetails.getUsername();
         Optional<Member> member = memberService.findByEmail(userEmail);
         List<String> hashTagList = hashTagRequestDto.getHashTagList();
+        List<HashTag> findHashTagList = hashTagService.findByHashTagNameList(hashTagList);
+        List<String> memberHashes = memberHashService.findHashByEmail(userEmail);
         if (hashTagList.size() != 0) {
-            for (String hashTag : hashTagList) {
-                Optional<HashTag> byHashTagName = hashTagService.findByHashTagName(hashTag);
-                MemberHash memberHashBuild = MemberHash.builder()
-                        .member(member.get())
-                        .hashTagId(byHashTagName.get())
-                        .build();
-                memberHashService.saveMemberHash(memberHashBuild);
-                log.info("{} 사용자가 해시태그 {}를 즐겨찾기에 추가하였습니다.",member.get().getNickname(), hashTag);
+            for (HashTag hashTag : findHashTagList) {
+                if (memberHashes.contains(hashTag.getHashTagName())){
+                    log.info("\"{}\" 해시태그는 이미 즐겨찾기 목록에 있습니다.",hashTag.getHashTagName());
+                }
+                else {
+                    MemberHash memberHashBuild = MemberHash.builder()
+                            .member(member.get())
+                            .hashTagId(hashTag)
+                            .build();
+                    memberHashService.saveMemberHash(memberHashBuild);
+                    log.info("{} 사용자가 해시태그 {}를 즐겨찾기에 추가하였습니다.", userEmail, hashTag.getHashTagName());
+                }
             }
         }
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Tag(name = "해시태그 관련 API")
+    @Operation(summary = "사용자 즐겨찾기 해시태그 삭제", description = "요청한 해시태그들을 사용자 즐겨찾기 해시태그 목록에서 삭제하는 API입니다.")
+    @ApiResponse(responseCode = "200", description = "사용자 즐겨찾기 해시태그가 삭제됩니다.")
     @DeleteMapping("/hashTag")
-    public ResponseEntity<?> deleteHashTag(@RequestBody HashTagRequestDto hashTagRequestDto){
-        String userEmail = tokenProvider.getUserEmail(hashTagRequestDto.getToken());
-        Optional<Member> member = memberService.findByEmail(userEmail);
+    public ResponseEntity<?> deleteHashTag(@Parameter(description = "삭제하려는 해시태그들") @RequestBody HashTagRequestDto hashTagRequestDto,
+                                           @AuthenticationPrincipal UserDetails userDetails){
+        String userEmail = userDetails.getUsername();
         List<String> hashTagList = hashTagRequestDto.getHashTagList();
         if (hashTagList.size() != 0) {
             for (String hashTag : hashTagList) {
@@ -86,11 +111,10 @@ public class HashTagController {
                     log.info("즐겨찾기된 해시태그가 없습니다.");
                 } else {
                     memberHashService.deleteMemberHash(memberHash.get());
+                    log.info("{} 사용자가 해시태그 {}를 즐겨찾기에 삭제하였습니다.",userEmail, hashTag);
                 }
-                log.info("{} 사용자가 해시태그 {}를 즐겨찾기에 삭제하였습니다.",member.get().getNickname(), hashTag);
             }
         }
-
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }

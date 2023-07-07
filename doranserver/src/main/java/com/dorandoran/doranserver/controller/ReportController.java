7de4a8db142e63b93df441d2d9c704c9ef1223ef
LeeKing2017 <thrusum123@gmail.com>
2023-07-07 17:28:44 +0900

    FEAT:댓글/대댓글 신고 기능 구현중

--- doranserver/src/main/java/com/dorandoran/doranserver/controller/ReportController.java
+++ doranserver/src/main/java/com/dorandoran/doranserver/controller/ReportController.java
@@ -1,12 +1,10 @@
 package com.dorandoran.doranserver.controller;
 
+import com.dorandoran.doranserver.dto.ReportCommentRequestDto;
+import com.dorandoran.doranserver.dto.ReportReplyRequestDto;
 import com.dorandoran.doranserver.dto.ReportRequestDto;
-import com.dorandoran.doranserver.entity.Member;
-import com.dorandoran.doranserver.entity.Post;
-import com.dorandoran.doranserver.entity.ReportPost;
-import com.dorandoran.doranserver.service.MemberService;
-import com.dorandoran.doranserver.service.PostService;
-import com.dorandoran.doranserver.service.ReportService;
+import com.dorandoran.doranserver.entity.*;
+import com.dorandoran.doranserver.service.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -26,6 +24,8 @@ public class ReportController {
     private final MemberService memberService;
     private final PostService postService;
     private final ReportService reportService;
+    private final CommentService commentService;
+    private final ReplyService replyService;
 
     @PostMapping("/Report")
     @Transactional
@@ -45,5 +45,33 @@ public class ReportController {
             post.setReportCount(post.getReportCount()+1);
             return new ResponseEntity<>(HttpStatus.OK);
         }
+    }
+
+    @PostMapping("/comment/report")
+    public ResponseEntity<?> saveReportComment(@RequestBody ReportCommentRequestDto reportCommentRequestDto,
+                                               @AuthenticationPrincipal UserDetails userDetails){
+        String userEmail = userDetails.getUsername();
+        Member member = memberService.findByEmail(userEmail)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 존재하지 않습니다."));
+        Comment comment = commentService.findCommentByCommentId(reportCommentRequestDto.getCommentId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
+
+        ReportComment reportComment = new ReportComment(comment, member, reportCommentRequestDto.getReportContent());
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/reply/report")
+    public ResponseEntity<?> saveReportReply(@RequestBody ReportReplyRequestDto reportReplyRequestDto,
+                                               @AuthenticationPrincipal UserDetails userDetails){
+        String userEmail = userDetails.getUsername();
+        Member member = memberService.findByEmail(userEmail)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 존재하지 않습니다."));
+        Reply reply = replyService.findReplyByReplyId(reportReplyRequestDto.getReplyId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
+
+        ReportReply reportReply = new ReportReply(reply, member, reportReplyRequestDto.getReportContent());
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
---
+++ doranserver/src/main/java/com/dorandoran/doranserver/dto/ReportCommentRequestDto.java
@@ -0,0 +1,15 @@
+package com.dorandoran.doranserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReportCommentRequestDto {
+    Long commentId;
+    String reportContent;
+}

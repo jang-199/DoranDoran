fea7bb8db715abaf24a1b31451473815839f8f57
LeeKing2017 <thrusum123@gmail.com>
2023-07-28 16:13:16 +0900

    REFACTOR:알림 내역 조회/읽기 때 반환 데이터 수정

--- doranserver/src/main/java/com/dorandoran/doranserver/dto/NotificationDto.java
+++ doranserver/src/main/java/com/dorandoran/doranserver/dto/NotificationDto.java
@@ -13,11 +13,8 @@ public class NotificationDto {
     @Setter
     @AllArgsConstructor
     @NoArgsConstructor
-    public static class notificationResponse{
+    public static class notificationResponse {
         Long notificationId;
-        Long postId;
-        Long commentId;
-        Long replyId;
         String title;
         String message;
         String notificationTime;
@@ -25,16 +22,34 @@ public class NotificationDto {
         NotificationType notificationType;
 
         @Builder
-        public notificationResponse(NotificationHistory notificationHistory, Long postId, Long commentId, Long replyId) {
+        public notificationResponse(NotificationHistory notificationHistory) {
             this.notificationId = notificationHistory.getNotificationHistoryId();
-            this.postId = postId;
-            this.commentId = commentId;
-            this.replyId = replyId;
             this.title = notificationTitle;
             this.message = notificationHistory.getMessage();
             this.isRead = notificationHistory.getNotificationReadTime() == null ? Boolean.FALSE : Boolean.TRUE;
             this.notificationTime = notificationHistory.getNotificationTime()
                     .format(DateTimeFormatter.ofPattern("MM/dd HH:mm"));
+            this.notificationType = notificationHistory.getNotificationType();
+        }
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class notificationReadResponse {
+        Long notificationId;
+        Long postId;
+        Long commentId;
+        Long replyId;
+        NotificationType notificationType;
+
+        @Builder
+        public notificationReadResponse(NotificationHistory notificationHistory, Long postId, Long commentId, Long replyId) {
+            this.notificationId = notificationHistory.getNotificationHistoryId();
+            this.postId = postId;
+            this.commentId = commentId;
+            this.replyId = replyId;
             this.notificationType = notificationHistory.getNotificationType();
         }
     }

d7242d0bea7834007968f9777932ceb3c3592d69
jang-199 <jw1010110@gmail.com>
2023-07-30 16:06:14 +0900

    REFACTOR: security url 변경

--- doranserver/src/main/java/com/dorandoran/doranserver/config/SecurityConfig.java
+++ doranserver/src/main/java/com/dorandoran/doranserver/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.dorandoran.doranserver.config.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,7 +28,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
 
         httpSecurity.authorizeHttpRequests(request->request
-                .requestMatchers("/api/nickname").permitAll()
+                .requestMatchers(HttpMethod.POST,"/api/nickname").permitAll()
                 .requestMatchers("/api/registered").permitAll()
                 .requestMatchers("/api/member").permitAll()
                 .requestMatchers("/api/token").permitAll()

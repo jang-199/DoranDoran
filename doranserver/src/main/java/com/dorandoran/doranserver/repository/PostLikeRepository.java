c653e9e7c745780fba0a4d4af7f5fd99822cbe29
jang-199 <jw1010110@gmail.com>
2023-08-06 01:32:04 +0900

    REFACTOR: 내가 좋아요한 글 목록 검색 로직 변경

--- doranserver/src/main/java/com/dorandoran/doranserver/repository/PostLikeRepository.java
+++ doranserver/src/main/java/com/dorandoran/doranserver/repository/PostLikeRepository.java
@@ -23,12 +23,12 @@ public interface PostLikeRepository extends JpaRepository<PostLike,Long> {
     Optional<PostLike> findLikeResult(@Param("email") String email, @Param("postId") Post postId);
 
     @Query("select p from PostLike p " +
-            "where p.memberId.email = :email " +
+            "where p.memberId.email = :email and p.checkDelete = false " +
             "order by p.postLikeId desc ")
     List<PostLike> findFirstMyLikedPosts(@Param("email") String email, PageRequest pageRequest);
 
     @Query("select p from PostLike p " +
-            "where p.memberId.email = :email and p.postLikeId <= :pos " +
+            "where p.memberId.email = :email and p.postLikeId <= :pos and p.checkDelete = false " +
             "order by p.postLikeId desc ")
     List<PostLike> findMyLikedPosts(@Param("email") String email,
                                     @Param("pos") Long position,

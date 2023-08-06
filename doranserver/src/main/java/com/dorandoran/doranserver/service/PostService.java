287edeee58855d9be5c08f78f7f6e483aa65fefa
jang-199 <jw1010110@gmail.com>
2023-08-06 23:34:43 +0900

    차단된 사용자 글 안보이게 쿼리 변경 v1

--- doranserver/src/main/java/com/dorandoran/doranserver/service/PostService.java
+++ doranserver/src/main/java/com/dorandoran/doranserver/service/PostService.java
@@ -1,6 +1,7 @@
 package com.dorandoran.doranserver.service;
 
 import com.dorandoran.doranserver.entity.Member;
+import com.dorandoran.doranserver.entity.MemberBlockList;
 import com.dorandoran.doranserver.entity.Post;
 
 import java.util.List;
@@ -9,9 +10,9 @@ import java.util.Optional;
 public interface PostService {
     public void savePost(Post post);
 
-    public List<Post> findFirstPost();
+    public List<Post> findFirstPost(List<MemberBlockList> memberBlockLists);
 
-    public List<Post> findPost(Long startPost);
+    public List<Post> findPost(Long startPost,List<MemberBlockList> memberBlockLists);
     public Post findSinglePost(Long postId);
     public void deletePost(Post post);
 

5bf490cb61f2ee5d72e28fcf8fe456c57a4462e9
LeeKing2017 <thrusum123@gmail.com>
2023-08-18 21:39:03 +0900

    REFACTOR: error log 코드 수정

--- doranserver/src/main/java/com/dorandoran/doranserver/service/FirebaseServiceImpl.java
+++ doranserver/src/main/java/com/dorandoran/doranserver/service/FirebaseServiceImpl.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
@@ -173,7 +174,7 @@ public class FirebaseServiceImpl implements FirebaseService {
         try{
             FirebaseMessaging.getInstance(firebaseConfig.aosFirebaseApp()).sendMulticastAsync(message);
         }catch (IOException e){
-            e.printStackTrace();
+            log.error(Arrays.toString(e.getStackTrace()));
         }
     }
 
@@ -181,7 +182,7 @@ public class FirebaseServiceImpl implements FirebaseService {
         try{
             FirebaseMessaging.getInstance(firebaseConfig.iosFirebaseApp()).sendMulticastAsync(message);
         }catch (IOException e){
-            e.printStackTrace();
+            log.error(Arrays.toString(e.getStackTrace()));
         }
     }
 }

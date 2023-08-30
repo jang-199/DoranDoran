18b3fef14339928e490f365eb8ca74781cd70134
jang-199 <jw1010110@gmail.com>
2023-08-30 11:01:38 +0900

    STYLE: 주석처리된 코드 주석 해제

--- doranserver/src/main/java/com/dorandoran/doranserver/domain/api/exception/ExceptionController.java
+++ doranserver/src/main/java/com/dorandoran/doranserver/domain/api/exception/ExceptionController.java
@@ -1,20 +1,20 @@
-//package com.dorandoran.doranserver.domain.api.exception;
-//
-//import com.dorandoran.doranserver.domain.api.post.controller.PostController;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//import org.springframework.web.multipart.MaxUploadSizeExceededException;
-//
-//@Slf4j
-//@RestControllerAdvice(assignableTypes = PostController.class)
-//public class ExceptionController {
-//
-//    @ExceptionHandler(MaxUploadSizeExceededException.class)
-//    public ResponseEntity<?> fileUploadException(Exception e){
-//        log.info("파일 업로드 크기 제한 exception",e);
-//        return new ResponseEntity<>("파일 업로드 크기 제한", HttpStatus.BAD_REQUEST);
-//    }
-//}
+package com.dorandoran.doranserver.domain.api.exception;
+
+import com.dorandoran.doranserver.domain.api.post.controller.PostController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+@Slf4j
+@RestControllerAdvice(assignableTypes = PostController.class)
+public class ExceptionController {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<?> fileUploadException(Exception e){
+        log.info("파일 업로드 크기 제한 exception",e);
+        return new ResponseEntity<>("파일 업로드 크기 제한", HttpStatus.BAD_REQUEST);
+    }
+}

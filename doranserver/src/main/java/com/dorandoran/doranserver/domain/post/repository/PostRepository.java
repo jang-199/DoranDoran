c50712416d3fe44bad580f937ae393c34a4545e1
jang-199 <jw1010110@gmail.com>
2023-09-17 15:54:29 +0900

    FIX<PostRepository>: isLocked 조회 안되게 옵션 변경

--- doranserver/src/main/java/com/dorandoran/doranserver/domain/post/repository/PostRepository.java
+++ doranserver/src/main/java/com/dorandoran/doranserver/domain/post/repository/PostRepository.java
@@ -15,18 +15,18 @@ import java.util.Optional;
 public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("select m from Post m " +
             "join fetch m.memberId " +
-            "where m.isLocked = false and m.memberId Not in :memberBlockLists and (m.memberId = :member and m.forMe = true) or (m.forMe = false) " +
+            "where m.isLocked = false and m.memberId Not in :memberBlockLists and ((m.memberId = :member and m.forMe = true) or (m.forMe = false)) " +
             "order by m.postId desc")
     List<Post> findFirstPostWithoutBlockLists(@Param("member") Member member, @Param("memberBlockLists") List<Member> memberBlockLists, PageRequest pageRequest);
     @Query("select m from Post m " +
             "join fetch m.memberId " +
-            "where m.isLocked = false and (m.memberId = :member and m.forMe = true) or (m.forMe = false) " +
+            "where m.isLocked = false and ((m.memberId = :member and m.forMe = true) or (m.forMe = false)) " +
             "order by m.postId desc")
     List<Post> findFirstPost(@Param("member") Member member, PageRequest pageRequest);
 
 
     @Query(value = "select m from Post m " +
-            "where m.postId <= :pos  and m.isLocked = false and m.memberId Not in :memberBlockLists and (m.memberId = :member and m.forMe = true) or (m.forMe = false) " +
+            "where m.postId <= :pos  and m.isLocked = false and m.memberId Not in :memberBlockLists and ((m.memberId = :member and m.forMe = true) or (m.forMe = false)) " +
             "order by m.postId desc ")
     List<Post> findPostWithoutBlockLists(@Param("pos") Long pos, @Param("member") Member member, @Param("memberBlockLists") List<Member> memberBlockLists, PageRequest pageRequest);
     @Query(value = "select m from Post m " +

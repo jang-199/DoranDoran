143dd9d66472823efb028fedd71e8bfcad14083c
LeeKing2017 <thrusum123@gmail.com>
2023-09-26 00:02:42 +0900

    FIX:댓글/대댓글 데이터 추가와 내용 변경 로직 추가 * 신고 누적으로 잠긴 댓글/대댓글 인지 확인 데이터 isLocked 추가 * 삭제된 댓글은 삭제된 댓글입니다.로 반환 * 신고 누적을 잠긴 댓글은 신고된 댓글입니다.로 반환

--- doranserver/src/main/java/com/dorandoran/doranserver/domain/comment/dto/ReplyDto.java
+++ doranserver/src/main/java/com/dorandoran/doranserver/domain/comment/dto/ReplyDto.java
@@ -38,24 +38,25 @@ public class ReplyDto {
         Boolean replyCheckDelete;
         Boolean isWrittenByMember;
         LocalDateTime replyTime;
+        Boolean isLocked;
 
         public ReadReplyResponse toEntity(Reply reply, Boolean isReplyWrittenByUser){
             return ReplyDto.ReadReplyResponse.builder()
                     .reply(reply)
-                    .content(reply.getReply())
                     .isWrittenByMember(isReplyWrittenByUser)
                     .build();
         }
 
         @Builder
-        public ReadReplyResponse(Reply reply, String content, Boolean isWrittenByMember) {
+        public ReadReplyResponse(Reply reply, Boolean isWrittenByMember) {
             this.replyId = reply.getReplyId();
             this.replyNickname = reply.getMemberId().getNickname();
-            this.reply = reply.getIsLocked().equals(Boolean.TRUE) ? "신고된 댓글입니다." : content;
+            this.reply = changeReplyByDeletedAndLocked(reply);
             this.replyAnonymityNickname = null;
             this.replyCheckDelete = reply.getCheckDelete();
             this.replyTime = reply.getCreatedTime();
             this.isWrittenByMember = isWrittenByMember;
+            this.isLocked = reply.getIsLocked();
         }
     }
     @Getter
@@ -86,5 +87,19 @@ public class ReplyDto {
             this.nickname = reply.getMemberId().getNickname();
             this.content = reply.getReply();
         }
+    }
+
+    private static String changeReplyByDeletedAndLocked(Reply reply){
+        String replyContent = reply.getReply();
+
+        if (reply.getIsLocked()){
+            replyContent = "신고된 댓글입니다.";
+        }
+
+        if (reply.getCheckDelete()){
+            replyContent = "삭제된 댓글입니다.";
+        }
+
+        return replyContent;
     }
 }

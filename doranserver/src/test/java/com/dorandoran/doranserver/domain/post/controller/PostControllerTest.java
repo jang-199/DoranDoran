023bd7cdbdd3feba404913db1b26c97396863d94
LeeKing2017 <thrusum123@gmail.com>
2023-10-03 21:48:41 +0900

    TEST<postController>:테스트 작성V1

--- doranserver/src/test/java/com/dorandoran/doranserver/domain/post/controller/PostControllerTest.java
+++ doranserver/src/test/java/com/dorandoran/doranserver/domain/post/controller/PostControllerTest.java
@@ -4,9 +4,11 @@ import com.dorandoran.doranserver.domain.comment.service.CommentLikeService;
 import com.dorandoran.doranserver.domain.comment.service.CommentService;
 import com.dorandoran.doranserver.domain.comment.service.ReplyService;
 import com.dorandoran.doranserver.domain.hashtag.service.HashTagService;
+import com.dorandoran.doranserver.domain.member.domain.Member;
 import com.dorandoran.doranserver.domain.member.service.LockMemberService;
 import com.dorandoran.doranserver.domain.member.service.MemberBlockListService;
 import com.dorandoran.doranserver.domain.member.service.MemberService;
+import com.dorandoran.doranserver.domain.notification.domain.osType.OsType;
 import com.dorandoran.doranserver.domain.notification.service.FirebaseService;
 import com.dorandoran.doranserver.domain.post.service.AnonymityMemberService;
 import com.dorandoran.doranserver.domain.post.service.PostHashService;
@@ -17,54 +19,84 @@ import com.dorandoran.doranserver.global.util.CommentResponseUtils;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest
 class PostControllerTest {
-    @Mock
+    @Value("${userUpload.Store.path}")
+    String userUploadPicServerPath;
+    @Value("${background.Store.path}")
+    String backgroundPicServerPath;
+    @Value("${doran.ip.address}")
+    String ipAddress;
+    @MockBean
     private MemberService memberService;
-    @Mock
+    @MockBean
     private PostLikeService postLikeService;
-    @Mock
+    @MockBean
     private HashTagService hashTagService;
-    @Mock
+    @MockBean
     private PostService postService;
-    @Mock
+    @MockBean
     private PostHashService postHashService;
-    @Mock
+    @MockBean
     private CommentService commentService;
-    @Mock
+    @MockBean
     private CommentLikeService commentLikeService;
-    @Mock
+    @MockBean
     private ReplyService replyService;
-    @Mock
+    @MockBean
     private AnonymityMemberService anonymityMemberService;
-    @Mock
+    @MockBean
     private LockMemberService lockMemberService;
-    @Mock
+    @MockBean
     private PostCommonService postCommonService;
-    @Mock
+    @MockBean
     private MemberBlockListService memberBlockListService;
-    @Mock
+    @MockBean
     private FirebaseService firebaseService;
-    @Mock
+    @MockBean
     private CommentResponseUtils commentResponseUtils;
 
     @Test
-    void savePost() {
+    void savePost() throws Exception {
+        //given
+        Member member = Member.builder()
+                .email("9643us@naver.com")
+                .dateOfBirth(LocalDate.now())
+                .firebaseToken("firebasetoken")
+                .closureDate(LocalDate.of(2000,12,12))
+                .nickname("doran")
+                .checkNotification(Boolean.TRUE)
+                .signUpDate(LocalDateTime.now())
+                .refreshToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqdzEwMTAxMTBAZ21haWwuY29tIiwiaWF0IjoxNjkxMjYwMjkzLCJleHAiOjE3MDY4MTIyOTMsInN1YiI6IuyImOyduCIsIlJPTEUiOiJST0xFX1VTRVIiLCJlbWFpbCI6Ijk2NDN1c0BuYXZlci5jb20ifQ.Jp88iBJy6OEfLyBGu8bQ9q8yAiQXi_M50syJJ5aTR0E")
+                .osType(OsType.Ios)
+                .build();
+
+
+
+
+        //when
+
+        //then
     }
 
     @Test
-    void postDelete() {
+    void postDelete() throws Exception {
     }
 
     @Test
-    void postLike() {
+    void postLike() throws Exception {
     }
 
     @Test
-    void postDetails() {
+    void postDetails() throws Exception {
     }
 }

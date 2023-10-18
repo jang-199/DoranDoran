15e28d8093d2471c861321afc06ea394faa4c21e
jang-199 <jw1010110@gmail.com>
2023-10-18 19:57:47 +0900

    FIX<AccountClosureMemberServiceImpl>: 회원 탈퇴 시 데이터베이스에 사진 경로도 함께 삭제되도록 변경

--- doranserver/src/test/java/com/dorandoran/doranserver/domain/member/controller/SignUpControllerTest.java
+++ doranserver/src/test/java/com/dorandoran/doranserver/domain/member/controller/SignUpControllerTest.java
@@ -18,18 +18,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcConfigurer;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;

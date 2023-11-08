9b6a4663ab29ab6a102a50f00a73dcd8df30d478
LeeKing2017 <thrusum123@gmail.com>
2023-11-08 22:25:17 +0900

    CHORE:Json Serailize, Deseiralize annotation 추가

--- doranserver/src/main/java/com/dorandoran/doranserver/domain/comment/dto/CommentDto.java
+++ doranserver/src/main/java/com/dorandoran/doranserver/domain/comment/dto/CommentDto.java
@@ -2,6 +2,10 @@ package com.dorandoran.doranserver.domain.comment.dto;
 
 import com.dorandoran.doranserver.domain.comment.domain.Comment;
 import com.dorandoran.doranserver.domain.comment.domain.Reply;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -44,6 +48,8 @@ public class CommentDto {
         private Boolean commentCheckDelete; //댓글 삭제 여부
         private int countReply;
         private Boolean isWrittenByMember; //내가 쓴 댓글인지 확인
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
         private LocalDateTime commentTime; //댓글 작성 시간
         private Boolean isLocked;
         private HashMap<String, Object> replies; //대댓글
